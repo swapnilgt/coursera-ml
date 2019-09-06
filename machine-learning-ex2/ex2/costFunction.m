92f87ea203bbdb260fc4cb95bb7f41b1c173e673
swapnil.g <swapnilg@zeta.tech>
2019-09-06 10:52:52 +0530

    Week 3 assignment

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -19,7 +19,19 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+HX = sigmoid(X * theta);
 
+indivCost = y .* log(HX) + (1 - y) .* log(1 - HX);
+
+J = (-1/m) * sum(indivCost);
+
+% Computing the grad.
+diff = HX - y;
+num_thetas = size(theta)(1,1);
+for thetaNum = 1:num_thetas
+    thetaMult = diff .* X(:,thetaNum);
+    grad(thetaNum, 1) = (1/m) * sum(thetaMult);
+end
 
 
 
--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -2,12 +2,12 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -17,8 +17,29 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+num_thetas = size(theta)(1,1);
 
+HX = sigmoid(X * theta);
 
+indivCost = y .* log(HX) + (1 - y) .* log(1 - HX);
+
+J = (-1/m) * sum(indivCost);
+
+% Now adding the lambda part to this.
+thetaSquareSum = (lambda/(2 * m)) * sum(theta(2:num_thetas) .^ 2)
+
+J = J + thetaSquareSum
+
+% Computing the grad.
+diff = HX - y;
+for thetaNum = 1:num_thetas
+    thetaMult = diff .* X(:,thetaNum);
+    if (thetaNum == 1)
+      grad(thetaNum, 1) = (1/m) * sum(thetaMult);
+    else
+      grad(thetaNum, 1) = (1/m) * sum(thetaMult) + (lambda/m) * theta(thetaNum, 1);
+    endif
+end
 
 
 
