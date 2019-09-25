85137ce25715732dfdb4a39842563f51274fbae0
swapnil.g <swapnilg@zeta.tech>
2019-09-25 10:21:34 +0530

    Exercise 3

---
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -0,0 +1,78 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters.
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Hint: The computation of the cost function and gradients can be
+%       efficiently vectorized. For example, consider the computation
+%
+%           sigmoid(X * theta)
+%
+%       Each row of the resulting matrix will contain the value of the
+%       prediction for that example. You can make use of this to vectorize
+%       the cost function and gradient computations.
+%
+% Hint: When computing the gradient of the regularized cost function,
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta;
+%           temp(1) = 0;   % because we don't add anything for j = 0
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+
+num_thetas = size(theta)(1,1);
+
+HX = sigmoid(X * theta);
+
+indivCost = y .* log(HX) + (1 - y) .* log(1 - HX);
+
+J = (-1/m) * sum(indivCost);
+
+% Now adding the lambda part to this.
+thetaSquareSum = (lambda/(2 * m)) * sum(theta(2:num_thetas) .^ 2);
+
+J = J + thetaSquareSum;
+
+% Computing the grad.
+diff = HX - y;
+grad = (1/m) * (X' * diff);
+
+% Now adding the regularization to the grad ...
+multiplier = ones(num_thetas, 1);
+multiplier(1, 1) = 0;
+
+grad = grad + (lambda/m) .* multiplier .* theta;
+
+% num_thetas = size(theta)(1,1);
+% for thetaNum = 1:num_thetas
+%    thetaMult = diff .* X(:,thetaNum);
+%    grad(thetaNum, 1) = (1/m) * sum(thetaMult);
+%end
+
+
+
+
+
+
+
+% =============================================================
+
+grad = grad(:);
+
+end
